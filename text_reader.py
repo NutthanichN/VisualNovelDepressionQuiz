ae3c9233f5addd3dbe7f0ed7dc4ad077d704d5e6
326th <43813462+326th@users.noreply.github.com>
2019-07-12 12:44:32 +0700

    add element

---
+++ text_reader.py
@@ -0,0 +1,28 @@
+class TextReader:
+    def __init__(self,start_directory):
+        self.directory = start_directory
+        self.dialog = []
+        self.index = 0
+        self.path = []  
+    def read_text(self,directory):
+        self.dialog = list(open(directory,'r').read().split('&'))
+        self.index = 0
+    def get_next_action(self):
+        if self.dialog[self.index][0] == '$':
+            return_value = ['D',[self.dialog[self.index][1:]]]
+        elif self.dialog[self.index][0] == '@':
+            return_value = list(self.dialog[self.index][1:].split('%'))
+            question = list(return_value[0].split('+'))
+            self.path = list(return_value[1].split('+'))
+            return_value = ['Q',question]
+        elif self.dialog[self.index][0] == '-':
+            return_value = ['S',[self.dialog[self.index][1:]]]
+        elif self.dialog[self.index][0] == '=':
+            return_value = ['P',[self.dialog[self.index][1:]]]
+        self.index += 1
+        return return_value
+    def change_path(self,question):
+        read_text(
+
+        
+        
